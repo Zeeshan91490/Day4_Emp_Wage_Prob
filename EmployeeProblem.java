7e3acd2df8a2a0ca138701bafe45e51f26f3f25a
Zeeshan91490 <www.zeeshankhan91490@gmail.com>
2020-10-02 13:43:12 +0530

    EmployeeProblem.java

--- EmployeeProblem.java
+++ EmployeeProblem.java
@@ -1,9 +1,24 @@
 package Day4EmpWageProb;
 
-public class EmployeeProblem {
+public class uc2EmpWage {
+
 	public static void main(String[] args){
-	      System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-	
-	}
-}
+		//Constants
+		int IS_FULL_TIME=1;
+		int EMP_RATE_PER_HOUR=20;
+		//Variables
+		int empHrs=0;
+		int empWage=0;
+		//Computation
+		        double empCheck = Math.floor(Math.random()*10)%2;
+			if (empCheck == IS_FULL_TIME )
+				empHrs = 8;
+			else
+				empHrs =0;
+			empWage=emHrs * EMP_RATE_PER_HOUR;
+			System.out.println("Employee wage:"+empWage);
+					
+				
+	}}
+
 
