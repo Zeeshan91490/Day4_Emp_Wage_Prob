c945079e4830477331059885f90474eb289814e5
Zeeshan91490 <www.zeeshankhan91490@gmail.com>
2020-10-02 13:58:27 +0530

    EmployeeProblem.java

--- EmployeeProblem.java
+++ EmployeeProblem.java
@@ -1,9 +1,39 @@
 package Day4EmpWageProb;
 
-public class EmployeeProblem {
-	public static void main(String[] args){
-	      System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-	
-	}
-}
+public class uc8EmpWage {
+	public static final int IS_FULL_TIME=2;
+	public static final int IS_PART_TIME=1;
 
+	public static int computeEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth){
+		//Variables
+		int empHrs=0;
+		int totalEmpHrs=0;
+		int totalWorkingDays=0;
+		//Computation
+		while(totalEmpHrs<= maxHoursPerMonth  && totalWorkingDays<numOfWorkingDays) {
+			totalWorkingDays++;
+		     int empCheck =(int) Math.floor(Math.random()*10)%3;
+				switch (empCheck) {
+				  case IS_PART_TIME:
+				     empHrs=4;
+				     break; 
+			      case IS_FULL_TIME:
+					     empHrs=8;
+					     break;
+			      default:
+			    	 empHrs=0;
+			    	 break;
+				}
+		 totalEmpHrs+= empHrs;
+		 System.out.println("Day:"+ totalWorkingDays+ ",EmpHrs::"+empHrs);
+			  
+	     }
+		int totalEmpWage= totalEmpHrs * empRatePerHour;
+		         System.out.println("Total Employee wage for company:"+company+"is:"+ totalEmpWage);
+		         return totalEmpWage;
+		         
+	}
+	public static void main(String[] args) {
+		computeEmpWage("DMart",20,2,10);
+		computeEmpWage("Reliance",10,4,20);
+	}}
