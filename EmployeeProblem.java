20ba61c46036ac8218dffc30438ee35888f7f1b1
Zeeshan91490 <www.zeeshankhan91490@gmail.com>
2020-10-02 13:36:39 +0530

    EmployeeProblem.java

---
+++ EmployeeProblem.java
@@ -0,0 +1,9 @@
+package Day4EmpWageProb;
+
+public class EmployeeProblem {
+	public static void main(String[] args){
+	      System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+	
+	}
+}
+
